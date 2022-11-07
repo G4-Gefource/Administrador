cef25f3d942abb15cd644f55eb94259588251287
Fiend3333 <116087739+Fiend3333@users.noreply.github.com>
2022-11-07 20:54:36 -0300

    mais algumas funções adicionadas

--- funcoes_adm.py
+++ funcoes_adm.py
@@ -17,7 +17,31 @@ def menu():  # FUNÇÃO PARA MENU
     print('2 - Remover \n')
     print('3 - Visualizar os serviços\n')
     print('0 - Sair\n')
-
+    
+#Função Olá mundo
 def apresentacao():
-    print("Olá, seja bem vindo ao setor administrativo do Eventum\n")
-    print("Por favor adicione as informaçoes do seu serviço a seguir\n")
+    print("Olá seja bem vindo ao setor administrativo do Eventum\n")
+    print("Por favor adicione as informações do seu serviço a seguir\n")
+
+#Função adicionar
+def adicionar(servico):
+    servico.append(input("Adicione as informações sobre o nome do serviço: "))
+
+    servico.append(input("Adicione as informações sobre o local do serviço: "))
+
+    servico.append(input("Adicione as informações o que ira prestar no serviço: "))
+
+    return servico
+
+#Função remover
+def remover(servico):
+    auxremover=input(f"Deseja remover {servico}?")
+    if auxremover[0] == 'S' or auxremover[0] == 's':
+        servico.clear()
+        print("O serviço 1 foi removido")
+    else:
+        print("O serviço não foi removido")
+    return servico
+    
+
+
