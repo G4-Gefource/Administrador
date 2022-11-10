105de0eb35914d6582e37308a590c92d4cb1e884
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2022-11-10 18:38:42 -0300

    atualização geral
    importação do novo código

--- funcoes_adm.py
+++ funcoes_adm.py
@@ -4,44 +4,26 @@ import msvcrt
 # FUNÇÕES
 
 
-def limpar():  # FUNÇÃO PARA LIMPAR A TELA
+def limpa():
     os.system("cls")
 
 
-def pausa():  # FUNÇÃO PARA PAUSAR O SISTEMA
+def pausa():
     print('\nPressione qualquer tecla para continuar...\n')
     char = msvcrt.getch()
 
-def menu():  # FUNÇÃO PARA MENU 
-    print('1 - Adicionar\n')
-    print('2 - Remover \n')
-    print('3 - Visualizar os serviços\n')
+
+def menu():
+    print('1 - Adicionar pontos turísticos\n')
+    print('2 - Atualizar ponto turístico\n')
+    print('3 - Visualizar pontos cadastrados\n')
+    print('4 - Remover ponto turísitico\n')
+    print('5 - Consultar notas do hotel/Eventum\n')
     print('0 - Sair\n')
-    
-#Função Olá mundo
-def apresentacao():
-    print("Olá seja bem vindo ao setor administrativo do Eventum\n")
-    print("Por favor adicione as informações do seu serviço a seguir\n")
-
-#Função adicionar
-def adicionar(servico):
-    servico.append(input("Adicione as informações sobre o nome do serviço: "))
-
-    servico.append(input("Adicione as informações sobre o local do serviço: "))
-
-    servico.append(input("Adicione as informações o que ira prestar no serviço: "))
-
-    return servico
-
-#Função remover
-def remover(servico):
-    auxremover=input(f"Deseja remover {servico}?")
-    if auxremover[0] == 'S' or auxremover[0] == 's':
-        servico.clear()
-        print("O serviço 1 foi removido")
-    else:
-        print("O serviço não foi removido")
-    return servico
-    
 
 
+def exibe_ponto(ponto):
+    print(f'Nome do estabelecimento: {(ponto["NOME"]).title()};')
+    print(f'Nota do estabelecimento: {ponto["NOTA"]};')
+    print(f'Localização do ponto: {ponto["LOCALIZAÇÃO"]} do hotel;')
+    print(f'Descrição do ponto: {ponto["DESCRIÇÃO"]};')
